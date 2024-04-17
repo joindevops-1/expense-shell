67cbe24f94cd7ccc3a2c1c083337a65f7967bb1e
joindevops <info@joindevops.com>
2024-04-17 20:19:53 +0530

    profile

--- frontend.sh
+++ frontend.sh
@@ -33,4 +33,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
 systemctl restart nginx
