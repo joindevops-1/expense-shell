940eb69cc494797bb02f829a7072f79575d80076
joindevops <info@joindevops.com>
2024-04-18 13:13:00 +0530

    functions

--- frontend.sh
+++ frontend.sh
@@ -26,12 +26,27 @@ else
     echo "You are super user."
 fi
 
-dnf install nginx -y 
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
 systemctl enable nginx
+VALIDATE $? "Enabling Nginx"
+
 systemctl start nginx
+VALIDATE $? "Starting Nginx"
+
 rm -rf /usr/share/nginx/html/*
+VALIDATE $? "Remove default site"
+
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "Download frontend code"
+
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+VALIDATE $? "Unzip frontend code"
+
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copied expenses conf"
+
 systemctl restart nginx
+VALIDATE $? "Restarted nginx"
