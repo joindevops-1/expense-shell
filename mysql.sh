8e6ab71105053b6dd2cdf4283ae9596d6b1beb4b
joindevops <info@joindevops.com>
2024-04-20 09:57:52 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USERID=$(id -u)
+source ./common.sh
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
@@ -20,15 +20,6 @@ VALIDATE(){
         echo -e "$2...$G SUCCESS $N"
     fi
 }
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
-fi
-
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Server"
