c42e5c82a3d9ba87461fcb9b6537aae26b84895f
joindevops <info@joindevops.com>
2024-04-20 10:00:55 +0530

    functions

--- common.sh
+++ common.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 USERID=$(id -u)
 check_root(){
     if [ $USERID -ne 0 ]
--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 source ./common.sh
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+
 echo "Please enter DB password:"
 read -s mysql_root_password
 
